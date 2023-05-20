7ebe77c3abec011f2cb03c85f8d1b9e1cbaec1ce
Konstantin-Volodin <63243086+Konstantin-Volodin@users.noreply.github.com>
2023-05-20 18:15:53 -0400

    setup assignment 1 and 2

---
+++ assignments/assignment_2/main.py
@@ -0,0 +1,21 @@
+#%% IMPORTS
+import pandas as pd
+import numpy as np
+import torch
+
+#%% PROCESS DATA
+
+
+df = pd.read_csv('data.csv')
+
+
+
+#%% TRANSFORM DATA
+
+
+#%% NEURAL NETWORK
+
+#%% EXPERIMENTATION (HYPER PARAMETERS)
+
+#%% EVALUATE THE MODEL
+
